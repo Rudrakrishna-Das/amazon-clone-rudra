11c638d54a5f95ba38ba3cda00d9b216b0657aed
Rudrakrishna-Das <rudradas16996@gmail.com>
2023-09-02 10:20:59 +0530

    app built

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,24 +1,39 @@
+import { useEffect, useState } from "react";
+
 import { StarIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
 
 const Products = ({ id, title, price, description, category, image }) => {
-  const [rating] = useState(Math.trunc(Math.random() * 5) + 1);
-  const [hasPrime] = useState(Math.random() < 0.5);
+  const [rating, setRating] = useState();
+  const [hasPrime, setHasPrime] = useState();
+
+  useEffect(() => {
+    setRating(Math.trunc(Math.random() * 5) + 1);
+    setHasPrime(Math.random() < 0.5);
+  }, []);
+
+  let currencyIND = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "INR",
+  });
+
+  const starRating = Array(rating)
+    .fill()
+    .map((_, i) => (
+      <StarIcon
+        key={i}
+        className="h-5 text-yellow-500"
+        suppressHydrationWarning
+      />
+    ));
   return (
     <div className="bg-white flex flex-col m-5 p-4 z-30">
       <p className="text-right text-xs italic text-gray-400">{category}</p>
       <img src={image} alt={title} className="h-28 mx-auto my-5" />
       <h4 className="my-4">{title}</h4>
-      {/* <div className="flex">   // ERROR REASON 1
-        {Array(rating)
-          .fill()
-          .map((_, i) => (
-            <StarIcon key={i} className="h-5 text-yellow-500" />
-          ))}
-      </div> */}
+      <div className="flex">{starRating}</div>
       <p className="text-xs my-2 line-clamp-2">{description}</p>
-      <div className="mb-5"></div>
-      {/* {hasPrime && (  // Error Reason 2
+      <div className="mb-5">{currencyIND.format(price)}</div>
+      {hasPrime && ( // Error Reason 2
         <div className="flex items-center space-x-2 -mt-5">
           <img
             className="w-12"
@@ -27,7 +42,7 @@ const Products = ({ id, title, price, description, category, image }) => {
           />
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
-      )} */}
+      )}
       <button className="mt-auto button">Add To Basket</button>
     </div>
   );
